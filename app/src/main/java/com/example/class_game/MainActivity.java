db577f0ac97b71016724263a237e66d9dd0bce00
Francis-Dwiputra-A <francis.dwiputra17@gmail.com>
2022-03-26 15:35:58 +0700

    Yes

--- app/src/main/java/com/example/class_game/MainActivity.java
+++ app/src/main/java/com/example/class_game/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.class_game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView pix_track;
+    TextView pix_track,txt_timer;
+    Button play;
     public class character {
         int att_melee, att_ranged, def_melee, def_ranged;
         public character(int att_m, int att_r, int def_m, int def_r){
@@ -24,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pix_track = findViewById(R.id.pixel_tracker);
+        txt_timer = findViewById(R.id.timer);
+        play = findViewById(R.id.play);
         pix_track.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -99,5 +104,39 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    public void play(View v){
+        start_timer();
+        play.setVisibility(View.INVISIBLE);
+    }
+    public void start_timer(){
+        boolean t = true;
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            public void run(){
+                int i = 0;
+                while(t){
+                    if(i >= 10){
+                        i = 0;
+                    }
+                    try{
+                        i++;
+                        Thread.sleep(1000);
+                    }
+                    catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
+                    int finalI = i;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            txt_timer.setText(""+ finalI);
+                        }
+                    });
+                }
+            }
+        };
+        new Thread(runnable).start();
     }
 }
