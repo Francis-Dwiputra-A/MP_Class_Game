61edc5d88323f60d551c02a90e9f6455b8192e9f
Francis-Dwiputra-A <francis.dwiputra17@gmail.com>
2022-04-11 19:52:29 +0700

    update interface

--- app/src/main/java/com/example/class_game/MainActivity.java
+++ app/src/main/java/com/example/class_game/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     TextView pix_track,txt_timer, txt_troop1, txt_troop2, txtinfo1, txtinfo2;
     private Toolbar mytopbar;
     Button play, resetbtn;
+    TextView piece1,piece2,piece3,piece4,piece5,piece6,piece7,piece8,piece9,piece10,piece11,piece12,piece13,piece14,piece15,piece16,piece17,piece18;
     int turn = 1, i = 0;
     character att = null,def = null;
     character S1_1,S2_1,S3_1,S1_2,S2_2,S3_2;
@@ -26,14 +27,16 @@ public class MainActivity extends AppCompatActivity {
         int hp;
         int price;
         int troops;
+        int pos;
 
-        public character(int ty,int a,int d, int h, int p, int t){
+        public character(int ty,int a,int d, int h, int p, int t, int ps){
             this.type = ty;
             this.att = a;
             this.def = d;
             this.hp = h;
             this.price = p;
             this.troops = t;
+            this.pos = ps;
         }
 
         public void setAtt(int att) {
@@ -74,6 +77,24 @@ public class MainActivity extends AppCompatActivity {
         play = findViewById(R.id.play);
         resetbtn = findViewById(R.id.restartbtn);
         mytopbar = findViewById(R.id.topBar);
+        piece1 = findViewById(R.id.btn1);
+        piece2 = findViewById(R.id.btn2);
+        piece3 = findViewById(R.id.btn3);
+        piece4 = findViewById(R.id.btn4);
+        piece5 = findViewById(R.id.btn5);
+        piece6 = findViewById(R.id.btn6);
+        piece7 = findViewById(R.id.btn7);
+        piece8 = findViewById(R.id.btn8);
+        piece9 = findViewById(R.id.btn9);
+        piece10 = findViewById(R.id.btn10);
+        piece11 = findViewById(R.id.btn11);
+        piece12 = findViewById(R.id.btn12);
+        piece13 = findViewById(R.id.btn13);
+        piece14 = findViewById(R.id.btn14);
+        piece15 = findViewById(R.id.btn15);
+        piece16 = findViewById(R.id.btn16);
+        piece17 = findViewById(R.id.btn17);
+        piece18 = findViewById(R.id.btn18);
         setSupportActionBar(mytopbar);
         createCharacter();
         pix_track.setOnTouchListener(new View.OnTouchListener() {
@@ -284,6 +305,118 @@ public class MainActivity extends AppCompatActivity {
                     }
                     txt_troop1.setText("Player 1 Troops Remaining: \nS1 = " + S1_1.troops + ";  S2 = " + S2_1.troops + ";  S3 = " + S3_1.troops + "\nM1 = " + M1_1.troops + ";  M2 = " + M2_1.troops + ";  M3 = " + M3_1.troops + " \nT1 = " +T1_1.troops + ";  T2 = " + T2_1.troops + ";  T3 = " + T3_1.troops);
                     txt_troop2.setText("Player 2 Troops Remaining: \nT1 = " +T1_2.troops + ";  T2 = " + T2_2.troops + ";  T3 = " + T3_2.troops + "\nM1 = " + M1_2.troops + ";  M2 = " + M2_2.troops + ";  M3 = " + M3_2.troops + " \nS1 = " + S1_2.troops + ";  S2 = " + S2_2.troops + ";  S3 = " + S3_2.troops);
+                    switch (att.pos){
+                        case 1:
+                            piece1.setText("S1 \n(" + att.troops + ")");
+                            break;
+                        case 2:
+                            piece2.setText("S2 \n(" + att.troops + ")");
+                            break;
+                        case 3:
+                            piece3.setText("S3 \n(" + att.troops + ")");
+                            break;
+                        case 4:
+                            piece4.setText("M1 \n(" + att.troops + ")");
+                            break;
+                        case 5:
+                            piece5.setText("M2 \n(" + att.troops + ")");
+                            break;
+                        case 6:
+                            piece6.setText("M3 \n(" + att.troops + ")");
+                            break;
+                        case 7:
+                            piece7.setText("T1 \n(" + att.troops + ")");
+                            break;
+                        case 8:
+                            piece8.setText("T2 \n(" + att.troops + ")");
+                            break;
+                        case 9:
+                            piece9.setText("T3 \n(" + att.troops + ")");
+                            break;
+                        case 10:
+                            piece10.setText("T1 \n(" + att.troops + ")");
+                            break;
+                        case 11:
+                            piece11.setText("T2 \n(" + att.troops + ")");
+                            break;
+                        case 12:
+                            piece12.setText("T3 \n(" + att.troops + ")");
+                            break;
+                        case 13:
+                            piece13.setText("M1 \n(" + att.troops + ")");
+                            break;
+                        case 14:
+                            piece14.setText("M2 \n(" + att.troops + ")");
+                            break;
+                        case 15:
+                            piece15.setText("M3 \n(" + att.troops + ")");
+                            break;
+                        case 16:
+                            piece16.setText("S1 \n(" + att.troops + ")");
+                            break;
+                        case 17:
+                            piece17.setText("S2 \n(" + att.troops + ")");
+                            break;
+                        case 18:
+                            piece18.setText("S3 \n(" + att.troops + ")");
+                            break;
+                    }
+                    switch (def.pos){
+                        case 1:
+                            piece1.setText("S1 \n(" + def.troops + ")");
+                            break;
+                        case 2:
+                            piece2.setText("S2 \n(" + def.troops + ")");
+                            break;
+                        case 3:
+                            piece3.setText("S3 \n(" + def.troops + ")");
+                            break;
+                        case 4:
+                            piece4.setText("M1 \n(" + def.troops + ")");
+                            break;
+                        case 5:
+                            piece5.setText("M2 \n(" + def.troops + ")");
+                            break;
+                        case 6:
+                            piece6.setText("M3 \n(" + def.troops + ")");
+                            break;
+                        case 7:
+                            piece7.setText("T1 \n(" + def.troops + ")");
+                            break;
+                        case 8:
+                            piece8.setText("T2 \n(" + def.troops + ")");
+                            break;
+                        case 9:
+                            piece9.setText("T3 \n(" + def.troops + ")");
+                            break;
+                        case 10:
+                            piece10.setText("T1 \n(" + def.troops + ")");
+                            break;
+                        case 11:
+                            piece11.setText("T2 \n(" + def.troops + ")");
+                            break;
+                        case 12:
+                            piece12.setText("T3 \n(" + def.troops + ")");
+                            break;
+                        case 13:
+                            piece13.setText("M1 \n(" + def.troops + ")");
+                            break;
+                        case 14:
+                            piece14.setText("M2 \n(" + def.troops + ")");
+                            break;
+                        case 15:
+                            piece15.setText("M3 \n(" + def.troops + ")");
+                            break;
+                        case 16:
+                            piece16.setText("S1 \n(" + def.troops + ")");
+                            break;
+                        case 17:
+                            piece17.setText("S2 \n(" + def.troops + ")");
+                            break;
+                        case 18:
+                            piece18.setText("S3 \n(" + def.troops + ")");
+                            break;
+                    }
                     i = 0;
                     if(turn == 1){
                         turn = 2;
@@ -311,24 +444,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void createCharacter(){
-        T1_1 = new character(1,35,75,70,100,5);
-        T2_1 = new character(1,35,75,70,100,5);
-        T3_1 = new character(1,35,75,70,100,5);
-        T1_2 = new character(1,35,75,70,100,5);
-        T2_2 = new character(1,35,75,70,100,5);
-        T3_2 = new character(1,35,75,70,100,5);
-        M1_1 = new character(1,35,75,70,100,4);
-        M2_1 = new character(1,35,75,70,100,4);
-        M3_1 = new character(1,35,75,70,100,4);
-        M1_2 = new character(1,35,75,70,100,4);
-        M2_2 = new character(1,35,75,70,100,4);
-        M3_2 = new character(1,35,75,70,100,4);
-        S1_1 = new character(2,90,30,45,130,5);
-        S2_1 = new character(2,90,30,45,130,5);
-        S3_1 = new character(2,90,30,45,130,5);
-        S1_2 = new character(2,90,30,45,130,5);
-        S2_2 = new character(2,90,30,45,130,5);
-        S3_2 = new character(2,90,30,45,130,5);
+        T1_1 = new character(1,35,75,70,100,5,7);
+        T2_1 = new character(1,35,75,70,100,5,8);
+        T3_1 = new character(1,35,75,70,100,5,9);
+        T1_2 = new character(1,35,75,70,100,5,10);
+        T2_2 = new character(1,35,75,70,100,5,11);
+        T3_2 = new character(1,35,75,70,100,5,12);
+        M1_1 = new character(1,35,75,70,100,4,4);
+        M2_1 = new character(1,35,75,70,100,4,5);
+        M3_1 = new character(1,35,75,70,100,4,6);
+        M1_2 = new character(1,35,75,70,100,4,13);
+        M2_2 = new character(1,35,75,70,100,4,14);
+        M3_2 = new character(1,35,75,70,100,4,15);
+        S1_1 = new character(2,90,30,45,130,5,1);
+        S2_1 = new character(2,90,30,45,130,5,2);
+        S3_1 = new character(2,90,30,45,130,5,3);
+        S1_2 = new character(2,90,30,45,130,5,16);
+        S2_2 = new character(2,90,30,45,130,5,17);
+        S3_2 = new character(2,90,30,45,130,5,18);
+        piece1.setText("S1 \n(" + S1_1.troops + ")");
+        piece2.setText("S2 \n(" + S2_1.troops + ")");
+        piece3.setText("S3 \n(" + S3_1.troops + ")");
+        piece4.setText("M1 \n(" + M1_1.troops + ")");
+        piece5.setText("M2 \n(" + M2_1.troops + ")");
+        piece6.setText("M3 \n(" + M3_1.troops + ")");
+        piece7.setText("T1 \n(" + T1_1.troops + ")");
+        piece8.setText("T2 \n(" + T2_1.troops + ")");
+        piece9.setText("T3 \n(" + T3_1.troops + ")");
+        piece10.setText("T1 \n(" + T1_2.troops + ")");
+        piece11.setText("T2 \n(" + T2_2.troops + ")");
+        piece12.setText("T3 \n(" + T3_2.troops + ")");
+        piece13.setText("M1 \n(" + M1_2.troops + ")");
+        piece14.setText("M2 \n(" + M2_2.troops + ")");
+        piece15.setText("M3 \n(" + M3_2.troops + ")");
+        piece16.setText("S1 \n(" + S1_2.troops + ")");
+        piece17.setText("S2 \n(" + S2_2.troops + ")");
+        piece18.setText("S3 \n(" + S3_2.troops + ")");
     }
 
     public void resetGame(View v){
